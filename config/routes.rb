9a2f1e1c9a3f46ef21720ad9ff04c90730fc0fc9
Mitch <a1770751@student.adelaide.edu.au>
2022-06-06 10:01:26 +0930

    add form for adding candidate - more structure work

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'voting#index'
   get '/voting/:id', to: 'voting#show'
   get '/addCandidate', to: 'voting#new', as: 'addCandidate'
-  post '/voting', to: 'voting'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
