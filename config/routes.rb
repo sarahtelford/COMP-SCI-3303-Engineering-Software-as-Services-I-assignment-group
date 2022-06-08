56ed4cbcf9a85aa54aac3a794bdf05c58873595e
Mitch <a1770751@student.adelaide.edu.au>
2022-06-08 10:06:27 +0930

    migrating databases

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get '/aboveLine' => 'candidates#aboveLine', as: :above_line
   get '/voting/:id' => 'candidates#show', as: :show
   get '/belowLine' => 'candidates#belowLine', as: :below_line
-  get '/addCandidate', to: 'candidates#new', as: :addCandidate
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
